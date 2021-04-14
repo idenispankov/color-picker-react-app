d7256009627646d6d3e2fc4d804a9dc9489a459f
Denis Pankov <idenispankov@gmail.com>
2021-04-14 13:56:26 -0700

    done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,36 @@
+import React, { useState } from 'react';
+import { SketchPicker } from 'react-color';
+
 function App() {
-  return <div className='page'>Hello</div>;
+  const [color, setColor] = useState('lightblue');
+  const [hidden, setHidden] = useState(false);
+
+  const pickerStyles = {
+    default: {
+      picker: {
+        position: 'absolute',
+        bottom: '60px',
+        left: '-40px',
+      },
+    },
+  };
+
+  return (
+    <div style={{ background: color }} className='page'>
+      <div className='page__container'>
+        {hidden && (
+          <SketchPicker
+            color={color}
+            styles={pickerStyles}
+            onChange={(updatedColor) => setColor(updatedColor.hex)}
+          />
+        )}
+        <button className='btn' onClick={() => setHidden(!hidden)}>
+          {hidden ? 'Close Color Picker' : 'Open Color Picker'}
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
